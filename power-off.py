f16ad89ca24bb414fd0cfe03c63ca8ffc8176d37
AlexBGoode <alexey@MacBook-Air-Alexey-2.local>
2015-12-28 17:07:55 +0300

    sudo apt-get install python-rpi.gpio

--- power-off.py
+++ power-off.py
@@ -1,6 +1,6 @@
 # Import the modules to send commands to the system and access GPIO pins
 from subprocess import call
-import RPi.GPIO as gpio
+import RPi.GPIO as GPIO
 
 # Define a function to keep script running
 def loop():
@@ -10,8 +10,19 @@ def loop():
 def shutdown(pin):
     call('halt', shell=False)
 
-gpio.setmode(gpio.BOARD) # Set pin numbering to board numbering
-gpio.setup(7, gpio.IN) # Set up pin 7 as an input
-gpio.add_event_detect(7, gpio.RISING, callback=shutdown, bouncetime=200) # Set up an interrupt to look for button presses
+# # Pin Setup:
+# GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+# GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
+# GPIO.setup(pwmPin, GPIO.OUT) # PWM pin set as output
+# pwm = GPIO.PWM(pwmPin, 50)  # Initialize PWM on pwmPin 100Hz frequency
+# GPIO.setup(butPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
+
+# $ sudo apt-get update
+# $ sudo apt-get dist-upgrade
+# $ sudo apt-get install python-rpi.gpio python3-rpi.gpio
+
+GPIO.setmode(gpio.BOARD) # Set pin numbering to board numbering
+GPIO.setup(7, GPIO.IN) # Set up pin 7 as an input
+GPIO.add_event_detect(7, GPIO.RISING, callback=shutdown, bouncetime=200) # Set up an interrupt to look for button presses
 
 loop() # Run the loop function to keep script running
